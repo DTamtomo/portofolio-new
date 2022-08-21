e7f1444a4a3cefad26c10315e4c932eb895b0e7f
Dwita327 <Dwitamtomo53@gmail.com>
2022-08-21 15:12:05 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*** typing animation***/
+var typed = new Typed(".typing",{
+    strings:["","Web Design","Beginner Front End Dev","UI/UX Design"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+function myalert() { 
+    alert("CV belum di bikin wkwkwk "); 
+} 
